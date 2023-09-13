ca3331db4b3e97559a7219678bdc873537b7b013
Refki Alfarizi <refkialfarizi46@gmail.com>
2023-09-13 23:17:33 +0700

    ganti format keterangan

--- modul1_tp/H01_19623034_02.py
+++ modul1_tp/H01_19623034_02.py
@@ -1,20 +1,25 @@
-# Problem 2
-# Program menerima input berupa dua buah titik pada bidang kartesius.
+# NIM/Nama: 19623034/Refki Alfarizi
+# Tanggal: 14 September 2023
+# Deskripsi: Program menerima input berupa dua buah titik pada bidang kartesius.
 # Program akan menentukan apakah garis yang melalui kedua titik tersebut
 # adalah garis horizontal, vertikal, atau garis bergradien tertentu.
 
-# Kamus
+# KAMUS
 # x1: Absis dari titik pertama (int)
 # y1: Ordinat dari titik pertama (int)
 # x2: Absis dari titik pertama (int)
 # y2: Ordinat dari titik kedua (int)
 # gradien: Kemiringan garis (float)
 
+# ALGORITMA
+
+# Input
 x1 = int(input("Masukkan x1: "))
 y1 = int(input("Masukkan y1: "))
 x2 = int(input("Masukkan x2: "))
 y2 = int(input("Masukkan y2: "))
 
+# Proses
 # Tentukan apakah gradien garis terdefinisi (Jika tidak maka garis vertikal)
 if x2 - x1 != 0:
   gradien = (y2 - y1) / (x2 - x1)
--- modul1_tp/H01_19623034_03.py
+++ modul1_tp/H01_19623034_03.py
@@ -1,18 +1,23 @@
-# Problem 3
-# Program menentukan pembagian kelas dan harga tiket pesawat
+# NIM/Nama: 19623034/Refki Alfarizi
+# Tanggal: 14 September 2023
+# Deskripsi: Program menentukan pembagian kelas dan harga tiket pesawat
 # berdasarkan kursi yang dipilih
 
-# Kamus
+# KAMUS
 # kelas: Kelas kursi yang dipilih (string)
 # harga: Harga kursi yang dipilih (int)
 # nomor: Nomor kursi yang dipilih (int)
 # posisi: Posisi kursi yang dipilih (string)
 
-kelas = ""
-harga = 0
+# ALGORITMA
 
+# Input
 nomor = int(input("Tentukan Nomor Kursi: "))
 posisi = input("Tentukan Posisi Kursi: ")
+
+# Proses
+kelas = ""
+harga = 0
 
 if (nomor > 0 and nomor <= 20) or (nomor > 50 and nomor <= 60):
   kelas = "Hot Seat"
